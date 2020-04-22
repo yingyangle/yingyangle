b9b041838e9ed200510f5b47fbd2573c9dffefe4
yingyangle <cyang1999@gmail.com>
2020-04-22 00:06:53 -0400

    lightbox

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// // collapse navbar when not in focus
+// $(function () { // document.addEventListener("DOMContentLoaded"
+
+//   // document.querySelector("#navbarToggle").addEventListener("blur",
+//   $("#hamburger").blur(function (event) {
+//     var screenWidth = window.innerWidth;
+//     if (screenWidth < 576) {
+//       $("#collapsed-nav-items").collapse('hide');
+//     }
+//   });
+
+//   // force focus clicked element in Firefox and Safari
+//   $("#hamburger").click(function (event) {
+//     $(event.target).focus();
+//   });
+// });
+
