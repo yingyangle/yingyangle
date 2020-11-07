e3875fe1abc1d398a14584290e35100ff783f51b
yingyangle <cyang1999@gmail.com>
2020-11-07 06:44:51 -0500

    reorganize tabs; add viz/art; new image lightbox

--- js/script.js
+++ js/script.js
@@ -1,18 +1,54 @@
+$('.nav-link, #title').on('click', function() {
+	window.location.reload(true);
+})
 
-// // collapse navbar when not in focus
-// $(function () { // document.addEventListener("DOMContentLoaded"
+$('#eng').on('click', function() {
+	window.location.href = ''; window.location.reload(true);
+})
 
-//   // document.querySelector("#navbarToggle").addEventListener("blur",
-//   $("#hamburger").blur(function (event) {
-//     var screenWidth = window.innerWidth;
-//     if (screenWidth < 576) {
-//       $("#collapsed-nav-items").collapse('hide');
-//     }
-//   });
+$('#de').on('click', function() {
+	window.location.href = '#de'; window.location.reload(true);
+})
 
-//   // force focus clicked element in Firefox and Safari
-//   $("#hamburger").click(function (event) {
-//     $(event.target).focus();
-//   });
-// });
 
+////////// IMAGE LIGHTBOX //////////
+
+// open overlay
+$('.image-container').on('click', function() {
+	// show overlay, disable background scroll
+	$('#overlay').css('visibility', 'visible')
+	overlay.setAttribute('aria-hidden', false)
+	document.body.classList.toggle('noscroll', true)
+
+	// show selected image
+	img = $(this).find('img').attr('src')
+	$('#image-overlay').attr('src', img)
+
+	// show caption for selected image
+	caption = $(this).find('img').attr('title')
+	$('#overlay-text').html(caption)
+	
+})
+
+// close overlay
+$('#overlay').on('click', function() {
+	// close overlay & re-enable scroll
+	$('#overlay').css('visibility', 'hidden')
+	document.body.classList.toggle('noscroll', false)
+
+	// zoom out image
+	$('#image-overlay').attr('width', '50%')
+})
+
+// image overlay zoom
+$('#image-overlay').on('click', function(e) {
+	e.stopPropagation() // don't trigger #overlay click event
+	// zoom in
+	if ($('#image-overlay').attr('width') == '50%') {
+		$('#image-overlay').attr('width', '70%')
+	} else { // zoom out
+		$('#image-overlay').attr('width', '50%')
+	}
+	
+	
+})
